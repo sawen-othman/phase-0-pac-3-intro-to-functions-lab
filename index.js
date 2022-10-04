b5647a5e372ffd7c7fc8e22e6a8e954364a0f6df
sawen <sawenothman2001@gmail.com>
2022-10-04 14:07:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 function shout(string) {
-    return string.toUpperCase()
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
   }
-  
-  function whisper(string) {
-    return string.toLowerCase()
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
   }
-  
-  function logShout(string) {
-    console.log(string.toUpperCase())
+
+  if (string === "Let's have dinner together!") {
+    return "I would love to!";
   }
-  
-  function logWhisper(string) {
-    console.log(string.toLowerCase())
-  }
-  
-  function sayHiToHeadphonedRoommate(string) {
-    if (string.toLowerCase() === string) {
-      return "I can't hear you!"
-    }
-  
-    if (string.toUpperCase() === string) {
-      return "YES INDEED!"
-    }
-  
-    if (string === "Let's have dinner together!") {
-      return "I would love to!"
-    }
-  }
+}
